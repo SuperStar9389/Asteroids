bf93e8b47f6f585f6f84fd0aea3fbcb007d3c1cc
SuperStar9389 <theodoreodysseus@outlook.com>
2025-10-25 08:34:35 -0400

    add player

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from constants import *
+from player import *
 import pygame
 
 def main():
@@ -8,9 +9,11 @@ def main():
     print("Screen height: " + str(SCREEN_HEIGHT))
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     while True:
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
