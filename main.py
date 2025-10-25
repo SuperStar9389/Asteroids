40e54743ef0ca2237be730085938902353610fa0
SuperStar9389 <theodoreodysseus@outlook.com>
2025-10-25 08:15:37 -0400

    black display

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from constants import *
 import pygame
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    print("Screen width: " + str(SCREEN_WIDTH))
+    print("Screen height: " + str(SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 
 if __name__ == "__main__":
