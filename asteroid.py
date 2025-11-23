5a904ce433a0c432ec98a9b0c0496ebaaea8d4f7
SuperStar9389 <theodoreodysseus@outlook.com>
2025-11-23 11:05:28 -0500

    change asteroid color

--- asteroid.py
+++ asteroid.py
@@ -6,7 +6,7 @@ class Asteroid(CircleShape):
 	def __init__(self, x, y, radius):
 		super().__init__(x, y, radius)
 	def draw(self, screen):
-		pygame.draw.circle(screen, 'white', (self.position[0], self.position[1]), self.radius, 2)
+		pygame.draw.circle(screen, 'red', (self.position[0], self.position[1]), self.radius, 2)
 	def update(self, dt):
 		self.position[0] += self.velocity[0] * dt
 		self.position[1] += self.velocity[1] * dt
