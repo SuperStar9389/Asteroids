30606e6526718ab72faf3974bf76724f9ed674c1
SuperStar9389 <theodoreodysseus@outlook.com>
2025-11-24 18:39:53 -0500

    add score

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pygame
 
 def main():
     pygame.init()
+    pygame.font.init()
     print("Starting Asteroids!")
     print("Screen width: " + str(SCREEN_WIDTH))
     print("Screen height: " + str(SCREEN_HEIGHT))
@@ -22,21 +23,29 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    score_font = pygame.font.Font(None, 36)
+    score = 0
     field = AsteroidField()
-    dt = 0
+    dt = -1
     while True:
         screen.fill("black")
+        score_text = score_font.render(f"Score: {score}", True, (255, 255, 255))
+        fps_text = score_font.render(f"FPS: {int(1 / dt)}", True, (255, 255, 255))
         updateable.update(dt)
         for sprite in drawable:
             sprite.draw(screen)
+        screen.blit(score_text, (10, 10))
+        screen.blit(fps_text, (10, 40))
         for asteroid in asteroids:
             if player.iscolliding(asteroid) and player.powerup_timer <= 0: 
                 print("Game over!")
                 return
+        
             for shot in shots:
                 if shot.iscolliding(asteroid):
                     shot.kill()
                     asteroid.split()
+                    score += SHOT_REWARD
         for powerup in powerups:
             if player.iscolliding(powerup):
                 player.powerup_timer = PLAYER_POWERUP_DURATION
